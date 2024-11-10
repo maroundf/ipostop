1d381327c3e9ad3d3167ef8d3cb394f871514742
DFREIHA <dfreiha@itrend.fr>
2024-11-10 18:22:35 +0100

    Update Podfile

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -13,10 +13,6 @@ echo "===== Verifying Node/NVM Versions ====="
 node -v
 npm -v
 
-echo "===== Setting NODE_BINARY ENV Variable ====="
-ln -s $(command -v node) /usr/local/bin/node
-export NODE_BINARY=$(command -v node)
-
 echo "===== Installing yarn ====="
 brew install yarn
 
@@ -31,19 +27,10 @@ ls -ltr
 
 echo "===== Installing dependencies ====="
 yarn install
-echo "===== Working Directory 2 ====="
-pwd
-ls -ltr node_modules
-if [ -d "node_modules/react-native" ]; then
-    ls -ltr node_modules/react-native
-    if [ -d "node_modules/react-native/scripts" ]; then
-        ls -ltr node_modules/react-native/scripts
-    fi
-fi
 
 echo "===== Changing Working Directory to ios ====="
 cd ios
-echo "===== Working Directory 3 ====="
+echo "===== Working Directory 2 ====="
 pwd
 ls -ltr
 
