ddce8b311af4d8397b9d882c8c85f95816dce22d
DFREIHA <dfreiha@itrend.fr>
2024-11-11 00:08:52 +0100

    Update Podfile

--- ios/ci_scripts/ci_pre_xcodebuild.sh
+++ ios/ci_scripts/ci_pre_xcodebuild.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-#export REACT_NATIVE_PATH="$PODS_ROOT/../../node_modules/react-native"
-export REACT_NATIVE_PATH="../node_modules/react-native"
+export REACT_NATIVE_PATH="/Volumes/workspace/repository/node_modules/react-native"
 echo "REACT_NATIVE_PATH is $REACT_NATIVE_PATH"
